a53a500827e5e8d8e282f5f347d09c592b489a0e
LXYYY <xiangyu002@e.ntu.edu.sg>
2020-06-01 13:44:25 +0800

    ddpg test added to compare

---
+++ multirobot/ddpg/environment.py
@@ -0,0 +1,49 @@
+from gym import Env as GymEnv
+
+from multirobot.environment import MultiAgentEnv as MaddpgEnv
+import numpy as np
+
+
+class BaseLinesEnv(GymEnv):
+    def __init__(self, maddpg_env):
+        if not isinstance(maddpg_env, MaddpgEnv):
+            raise NotImplementedError
+        else:
+            super(BaseLinesEnv, self).__init__()
+            self.maddpg_env = maddpg_env
+            self.action_space = maddpg_env.action_space
+            self.observation_space = maddpg_env.observation_space
+            self.n = self.maddpg_env.n
+            # self.action_space_n_shape = np.zeros(maddpg_env.action_space[0].shape[-1]*self.n,)
+            # self.observation_space_n_shape = (maddpg_env.observation_space[0].shape[-1] * self.n,)
+            self.action_space_n_shape = (self.n, maddpg_env.action_space[0].shape[-1])
+            self.observation_space_n_shape = (
+                self.n, maddpg_env.observation_space[0].shape[-1])
+            self.reward_shape = (self.n, 1)
+            self.terminal_shape = (self.n, 1)
+
+    def step(self, action_n):
+        action_n = [action.reshape(2) for action in action_n]
+        obs_n, rew_n, done_n, info_n = self.maddpg_env.step(action_n)
+        obs_n = self.obs_reshape(obs_n)
+        rew_n = np.vstack(rew_n)
+        done_n = np.vstack(done_n)
+        info_n = np.vstack(info_n)
+        return obs_n, rew_n, done_n, info_n
+
+    def reset(self):
+        obs_n = self.maddpg_env.reset()
+        return self.obs_reshape(obs_n)
+
+    def reset_vehicle(self, agent_id=None):
+        self.maddpg_env.reset_vehicle(agent_id)
+
+    def render(self, mode='human'):
+        self.maddpg_env.render()
+
+    def close(self):
+        pass
+
+    @staticmethod
+    def obs_reshape(obs_n):
+        return np.vstack([obs.reshape(1, obs.shape[0]) for obs in obs_n])
